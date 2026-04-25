b47f68de535d0abb4b72dff9e24aed4bb5469b5e
Kunwei Zhang <kunweiz92@gmail.com>
2026-04-25 18:37:56 +0800

    chore: add install and remove scripts for gsd modules

---
+++ install.sh
@@ -0,0 +1,4 @@
+gsd install https://github.com/PamelaSprin47685ghall/gsd-context-prune
+gsd install https://github.com/PamelaSprin47685ghall/gsd-auto-continue
+gsd install https://github.com/PamelaSprin47685ghall/gsd-explicit-reactive
+gsd install https://github.com/PamelaSprin47685ghall/gsd-hints-injector
---
+++ remove.sh
@@ -0,0 +1,4 @@
+gsd remove https://github.com/PamelaSprin47685ghall/gsd-context-prune
+gsd remove https://github.com/PamelaSprin47685ghall/gsd-auto-continue
+gsd remove https://github.com/PamelaSprin47685ghall/gsd-explicit-reactive
+gsd remove https://github.com/PamelaSprin47685ghall/gsd-hints-injector
