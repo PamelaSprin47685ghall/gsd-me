817f7e2f727bee3be897e688a7d72ce69a27b0a5
Kunwei Zhang <kunweiz92@gmail.com>
2026-04-29 08:25:15 +0800

    chore: auto-commit after complete-milestone
    GSD-Unit: M001

---
+++ security_audit_2.py
@@ -0,0 +1,18 @@
+import os
+
+files = [
+    "gsd-trueline/src/common/security.js",
+    "gsd-trueline/src/common/hash.js",
+    "gsd-trueline/src/common/parse.js",
+    "gsd-trueline/src/read/reader.js",
+    "gsd-trueline/src/read/line-splitter.js",
+]
+
+for f in files:
+    if os.path.exists(f):
+        print(f"--- START OF FILE: {f} ---")
+        with open(f, 'r') as file:
+            print(file.read())
+        print(f"--- END OF FILE: {f} ---\n")
+    else:
+        print(f"--- FILE NOT FOUND: {f} ---\n")
